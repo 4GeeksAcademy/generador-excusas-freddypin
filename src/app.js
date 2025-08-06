6cbd9ecfdaf7f223ded4ceaad9216bba5aa59b33
freddypg9 <freddypg96@hotmail.com>
2025-08-06 18:59:58 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = () => {
+  const who = ['Mi gato', 'Un novato', 'El entrenador', 'El dueño', 'Mi abuela'];
+  const actions = ['vomitó sobre', 'se tropezó con', 'rompió', 'lanzó accidentalmente', 'escondió'];
+  const objects = ['mis guantes', 'mi protector bucal', 'mi rodillera', 'mi mochila', 'el vendaje'];
+  const when = ['justo antes del sparring', 'durante el calentamiento', 'mientras me vendaba', 'cuando llegué al gimnasio', 'en el vestuario'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const getRandomItem = (array) => array[Math.floor(Math.random() * array.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const generarExcusa = () => {
+    const sujeto = getRandomItem(who);       
+    const verbo = getRandomItem(actions);    
+    const objeto = getRandomItem(objects);  
+    const momento = getRandomItem(when);     
+
+    return `${sujeto} ${verbo} ${objeto} ${momento}.`;
+  };
+ document.getElementById("excuse").innerText = generarExcusa();
 };
